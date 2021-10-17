85abc255bf86001c3894dfd3d0e889a142668b0d
aa870111 <asz18369@gmail.com>
2021-10-17 14:55:40 +0800

    10/20 HW

---
+++ src/components/CollectionOverview_40.js
@@ -0,0 +1,13 @@
+import React from "react";
+import'./CollectionOverview_40.scss'
+import CollectionPreview_40 from "./CollectionPreview_40";
+
+const CollectionOverview_40 = () => {
+    return (
+    <div class="collection-overview">
+      <CollectionPreview_40 />
+    </div>
+    );
+};
+
+export default CollectionOverview_40;
---
+++ src/pages/ShopOverviewPage_40.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CollectionOverview_40 from "../components/CollectionOverview_40";
+
+const  ShopOverviewPage_40 = () => {
+    return (
+        <CollectionOverview_40 />
+    )
+}
+
+export default ShopOverviewPage_40;
