b4451bd507cee3737e1cf9379da0aedf30e0ed9f
aa870111 <asz18369@gmail.com>
2021-10-23 10:17:37 +0800

    github page

--- src/App_40.js
+++ src/App_40.js
@@ -1,14 +1,16 @@
 import './App_40.scss';
 import Homepage_40 from './pages/Homepage_40';
-import {Switch,Route} from 'react-router-dom'
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import ShopPage_40 from './pages/ShopPage_40';
 import SigninPage_40 from './pages/SigninPage_40';
 import ContactPage_40 from './pages/ContactPage_40';
 import Header_40 from './components/Header_40';
 
+
 function App_40() {
   return (
     <div>
+      <BrowserRouter>
       <Header_40 />
       <Switch>
       <Route exact path='/'component={Homepage_40} />
@@ -16,6 +18,7 @@ function App_40() {
       <Route exact path='/contact_40' component={ContactPage_40} />
       <Route exact path='/signin_40' component={SigninPage_40} />
       </Switch>
+      </BrowserRouter>
     </div>
   );
 }
